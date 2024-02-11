942655e3b4623bef18773f03c6bfdd285def0e89
Rompetr78 <rompetr@yahoo.com>
2024-02-11 19:36:47 +0800

    added first task in /seminar_2/Program.cs

--- seminar_2/Program.cs
+++ seminar_2/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая принимает на вход
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// Примеры
+// a = 256 => 26
+// a = 891 => 81
+
+int number = 947;
+int firstDigit = number / 100; // 453 / 100 = 4
+int thirdDigit = number % 10; // 453 % 10 = 3
+int result = firstDigit * 10 + thirdDigit; // 4 * 10 + 3 = 43
+// $"Текст {result} текст {a+100} текст {a - 500}"
+Console.WriteLine($"В числе {number} => {result}");
