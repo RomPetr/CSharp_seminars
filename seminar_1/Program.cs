2f7b4de7d18abe3101572ca633e482036137c1f1
Rompetr78 <rompetr@yahoo.com>
2024-02-11 19:12:23 +0800

    added folder seminar_1

---
+++ seminar_1/Program.cs
@@ -0,0 +1,35 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Console.WriteLine("Hello, World!");
+// int a = 40;
+// int b = 5;
+
+// if (a == b * b)
+// {
+//     Console.WriteLine("Yes");
+// }
+// else
+// {
+//     Console.WriteLine("No");
+// }
+
+// using System.ComponentModel;
+
+// int N = 4;
+//  int negaN = -N;
+//  while(negaN <= N)
+//  {
+//     Console.Write(negaN + " ");
+//     negaN++;
+//  }
+
+      int number = 6;
+      int even = 1;
+      while(even <= number)
+      {
+        if(even%2 == 0)
+        {
+          Console.Write(even + " ");
+        }
+        even++;
+      }
+   
