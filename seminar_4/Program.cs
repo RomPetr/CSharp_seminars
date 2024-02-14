26bb162d88773b8cd6622a1d19b4d4f1ba3cd02e
Rompetr78 <rompetr@yahoo.com>
2024-02-14 15:13:42 +0800

    added second task of /seminar_4/Program.cs

--- seminar_4/Program.cs
+++ seminar_4/Program.cs
@@ -1,4 +1,4 @@
-﻿/* Задачи семинара */
+﻿/* Задача 1 семинара 
 // Задайте массив из N случайных целых чисел (N вводится с
 // клавиатуры).
 // Найдите количество чисел, которые оканчиваются на 1 и
@@ -31,3 +31,40 @@ int N = Convert.ToInt32(Console.ReadLine());
 int[] result_arr = CreateArray(N); //создан массив случайных чисел из N элементов
 Console.WriteLine ($"Массив: [ {string.Join(";", result_arr)} ]");
 Console.WriteLine ($"Результат: {GetCount(result_arr)}");
+*/
+//---------------------------------------------------------------
+/* Задача 2 семинара */
+// Заполните массив на N (вводится с консоли, не более 8)
+// случайных целых чисел от 0 до 9.
+// Сформируйте целое число, которое будет состоять из цифр из
+// массива. Старший разряд числа находится на 0-м индексе,
+// младший – на последнем.
+// Пример
+// [1 3 2 4 2 3] => 132423
+// [2 3 1] => 231
+
+int[] CreateArray (int size)
+{
+    int[] array = new int[size];
+    for(int i =0; i<array.Length; i++)
+    {
+        array[i] = new Random().Next(10);
+    }
+    return array;
+}
+
+int ConvertArrayToInteger (int[] arr)
+{
+    int res = 0;
+    for (int i = 0, j = arr.Length-1; i<arr.Length; i++, j--)
+    {
+        res = res + arr[i] * (int)Math.Pow(10, j);
+    }
+    return res;
+}
+
+Console.Write ("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int[] result_arr = CreateArray(N); //создан массив случайных чисел из N элементов
+Console.WriteLine ($"Массив: [ {string.Join(";", result_arr)} ]");
+Console.WriteLine ($"Результат: {ConvertArrayToInteger(result_arr)}");
