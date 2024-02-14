57e4e10343573381e944ae1eb9f54d482dd957ae
Rompetr78 <rompetr@yahoo.com>
2024-02-15 02:51:50 +0800

    Added the Lesson04/ folder with first task in Program.cs

---
+++ Lesson04/Program.cs
@@ -0,0 +1,18 @@
+﻿//Создание двумерного массива
+Random rnd = new Random();
+int[,] matrix = new int [7,8];
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    matrix[i,j] = rnd.Next(1, 10);
+}
+
+for (int i = 0; i < matrix.GetLength(0); i++)
+{
+    for (int j = 0; j < matrix.GetLength(1); j++)
+    {
+    Console.Write ($"{matrix[i,j]} ");
+    }
+    Console.WriteLine();
+}
