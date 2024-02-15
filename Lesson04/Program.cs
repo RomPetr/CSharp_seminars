2c797f7c28d69959f587285cac53912ecf3fadeb
Rompetr78 <rompetr@yahoo.com>
2024-02-15 12:36:20 +0800

    added the second task of /Lesson04/Program.cs

--- Lesson04/Program.cs
+++ Lesson04/Program.cs
@@ -18,9 +18,10 @@ for (int i = 0; i < matrix.GetLength(0); i++)
 }
 */
 //---------------------------------------------------------------
-/* Создание и вывод на экран двумерного массива через функции */
+/* Задача 1: 
+//Создание и вывод на экран двумерного массива через функции 
 
-int[,] createMatrix (int rowCount, int columsCount)
+int[,] CreateMatrix (int rowCount, int columsCount)
 {
     Random rnd = new Random();
     int[,] matrix = new int[columsCount, rowCount];
@@ -33,7 +34,7 @@ int[,] createMatrix (int rowCount, int columsCount)
     return matrix;
 }
 
-void showMatrix(int[,] matrix)
+void ShowMatrix(int[,] matrix)
 {
     for (int i = 0; i < matrix.GetLength(0); i++)
     {
@@ -46,5 +47,64 @@ void showMatrix(int[,] matrix)
 
 }
 
-int[,] array = createMatrix (4,5);
-showMatrix (array);
+int[,] array = CreateMatrix (4,5);
+ShowMatrix (array);
+*/
+//---------------------------------------------------------------
+/* Задача 2: */
+//Создать двумерный массив, состоящий из целых чисел.
+//Вывести на экран "интересные" элементы массива.
+//Назовем число интересным, если сумма его цифр четная
+
+int[,] CreateMatrix (int rowCount, int columsCount)
+{
+    Random rnd = new Random();
+    int[,] matrix = new int[columsCount, rowCount];
+
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+            matrix[i, j] = rnd.Next(1, 1000);
+    }
+    return matrix;
+}
+
+void ShowMatrix(int[,] matrix)
+{
+    for (int i = 0; i < matrix.GetLength(0); i++)
+    {
+        for (int j = 0; j < matrix.GetLength(1); j++)
+        {
+            Console.Write($"{matrix[i, j]} ");
+        }
+        Console.WriteLine();
+    }
+
+}
+
+int[,] array = CreateMatrix (4,5);
+ShowMatrix (array);
+Console.WriteLine ();
+
+foreach (int e in array)
+{
+    if (IsInteresting (e)) Console.WriteLine (e);
+}
+
+bool IsInteresting (int value)
+{
+    int sumOfDigits = GetSumOfDigits (value);
+    if (sumOfDigits % 2 == 0) return true;
+    return false;
+}
+
+int GetSumOfDigits (int value)
+{
+    int sum = 0;
+    while (value > 0)
+    {
+        sum += value % 10;
+        value /= 10;
+    }
+    return sum;
+}
