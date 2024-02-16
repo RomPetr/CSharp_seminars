dfab5f8668b0bfa7c3b0f3d7cd310b8215f53727
Rompetr78 <rompetr@yahoo.com>
2024-02-16 23:14:24 +0800

    added the fourth task of /Lesson04/Program.cs

--- Lesson04/Program.cs
+++ Lesson04/Program.cs
@@ -110,7 +110,7 @@ int GetSumOfDigits (int value)
 }
 */
 
-/* Задача 3: */
+/* Задача 3: 
 //Ввести строку состоящию из букв и цифр и оставить только буквы
 
 string GetOfLettersFromString (string s)
@@ -129,3 +129,60 @@ string GetOfLettersFromString (string s)
 string str = Console.ReadLine ();
 string result = GetOfLettersFromString (str);
 Console.WriteLine (result);
+*/
+//---------------------------------------------------------------
+/* Задача 4:*/
+//Считать с консоли строку, состоящую из цифр и латинских букв.
+//Сформировать массив, состоящий из цифр этой строки.
+//Пример: abc123def06g => [1, 2, 3, 0, 6]
+
+ int GetOfNumbersFromString (string s)
+{
+    string digits = "";
+    foreach (char e in s)
+    {
+        if (char.IsAsciiDigit (e) == true)
+        {
+            digits += e;
+        }
+    }
+    int number = Convert.ToInt32 (digits);
+    return number;
+}
+
+int CalculateMatrixSize (int n)
+{
+    int number = n;
+    int count = 0;
+    while(number > 0)
+    {
+        number /= 10;
+        count++;
+    }
+    return count;
+}
+
+void CreateDigitMatrix (int[] arr, int res_num)
+{
+    int stepen = arr.Length - 1;
+    int number = (int) Math.Pow (10, stepen);
+    for (int i = 0; i < arr.Length; i++)
+    {
+        arr[i] = (res_num / number);
+        res_num = res_num - (arr[i] * number);
+    
+        number /= 10;
+    }
+    Console.Write($"[ {string.Join(";", arr)} ]");
+}
+
+Console.WriteLine ("Please, enter the string of letters and numbers:");
+string str = Console.ReadLine ();
+int result_number = GetOfNumbersFromString (str);
+//Console.WriteLine (result_number);
+int N = CalculateMatrixSize (result_number);
+//Console.WriteLine (N);
+int[] arr = new int[N];
+
+CreateDigitMatrix (arr, result_number);
+Console.WriteLine ();
