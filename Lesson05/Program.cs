bf402c584ac03dd7f60115895ebe1f2261c66255
Rompetr78 <rompetr@yahoo.com>
2024-02-18 11:55:41 +0800

    Added the /Lesson05 folder with first task in Program.cs

---
+++ Lesson05/Program.cs
@@ -0,0 +1,13 @@
+﻿/* Рекурсия */
+/* Задача 1 */
+// Найти факториал числа N
+
+int Fact(int n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    return n * Fact(n - 1);
+}
+
+int N = 5;
+Console.WriteLine(Fact(N));
