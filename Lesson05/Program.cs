0e5bbab48a7cff85fe7bb6e33c0623df19ec11f1
Rompetr78 <rompetr@yahoo.com>
2024-02-18 12:06:36 +0800

    added subtask in first task in /Lesson05/Program.cs

--- Lesson05/Program.cs
+++ Lesson05/Program.cs
@@ -2,12 +2,25 @@
 /* Задача 1 */
 // Найти факториал числа N
 
-int Fact(int n)
-{
-    if (n == 1 || n == 0)
-        return 1;
-    return n * Fact(n - 1);
-}
+// int FactRecurs(int n)
+// {
+//     if (n == 1 || n == 0)
+//         return 1;
+//     return n * Fact(n - 1);
+// }
 
-int N = 5;
-Console.WriteLine(Fact(N));
+// int N = 10;
+// Console.WriteLine(FactRecurs(N));
+//................................
+//Решение через цикл for
+int FactСycle(int n)
+{
+    int result = 1;
+    for (int i = 2; i <= n; i++)
+    {
+        result *= i;
+    }
+    return result;
+}
+int N = 10;
+Console.WriteLine(FactСycle(N));
