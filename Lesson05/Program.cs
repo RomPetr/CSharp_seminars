bdcad085868c6ac3a9503d209f4844c63866b1d8
Rompetr78 <rompetr@yahoo.com>
2024-02-19 00:21:53 +0800

    edited task1 in /Lesson05/Program.cs

--- Lesson05/Program.cs
+++ Lesson05/Program.cs
@@ -1,19 +1,19 @@
 ﻿/* Рекурсия */
-/* Задача 1 
+/* Задача 1 */
 // Найти факториал числа N
 
-// int FactRecurs(int n)
-// {
-//     if (n == 1 || n == 0)
-//         return 1;
-//     return n * FactRecurs(n - 1);
-// }
+int FactRecurs(int n)
+{
+    if (n == 1 || n == 0)
+        return 1;
+    return n * FactRecurs(n - 1);
+}
 
-// int N = 10;
-// Console.WriteLine(FactRecurs(N));
+int N = 10;
+Console.WriteLine(FactRecurs(N));
 //................................
 //Решение через цикл for
-int FactСycle(int n)
+/*int FactСycle(int n)
 {
     int result = 1;
     for (int i = 2; i <= n; i++)
@@ -36,31 +36,31 @@ Console.WriteLine(FactСycle(N));
 //Возврат: n=4; fact=6
 //Возврат: n=5; fact=24
 
-int count = 0;
+// int count = 0;
 
-int FactRecurs(int n)
-{
-    count++;
-    if (n == 1 || n == 0)
-    {
-        //Console.WriteLine($"Stop reqursion: n={n}");
-        return 1;
-    }
-    //Console.WriteLine(n);
+// int FactRecurs(int n)
+// {
+//     count++;
+//     if (n == 1 || n == 0)
+//     {
+//         //Console.WriteLine($"Stop reqursion: n={n}");
+//         return 1;
+//     }
+//     //Console.WriteLine(n);
 
-    return n * FactRecurs(n-1);
+//     return n * FactRecurs(n-1);
 
-}
+// }
 
-Console.Write("Введите число для вычисления его факториала: ");
-int N = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Результат:");
-Console.WriteLine(FactRecurs(N));
-Console.WriteLine("Отладка:");
-int num = count;
-for (int i = 1; i <= num; i++)
-{
-    Console.WriteLine ($"Возврат: n = {i}; fact = {FactRecurs(i)}");
-}
-//Console.WriteLine(count);
+// Console.Write("Введите число для вычисления его факториала: ");
+// int N = Convert.ToInt32(Console.ReadLine());
+// Console.WriteLine("Результат:");
+// Console.WriteLine(FactRecurs(N));
+// Console.WriteLine("Отладка:");
+// int num = count;
+// for (int i = 1; i <= num; i++)
+// {
+//     Console.WriteLine ($"Возврат: n = {i}; fact = {FactRecurs(i)}");
+// }
+// //Console.WriteLine(count);
 
