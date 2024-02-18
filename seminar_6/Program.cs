18fbac1dc5ddc28f41f313187b009ff4053a88ba
Rompetr78 <rompetr@yahoo.com>
2024-02-19 01:02:11 +0800

    added the first task of /seminar_6/Program.cs

--- seminar_6/Program.cs
+++ seminar_6/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача 1 */
+//Задайте массив символов (тип char[]).
+//Создайте строку из символов этого массива
+//Пример:
+// [a, b, c, d, e] => "abcde"
+
+string GetStringFromCharArray (char[] array)
+{
+    string result = string.Empty; //""
+    foreach (char symbol in array)
+    // symbol = 'a', symbol = 'b', symbol = 'c', symbol = 'd'
+    {
+        result = result + symbol;
+    }
+    return result;
+}
+
+char[] chars =  {'1', 'd', '!', '2', 'f'};
+Console.WriteLine();
+string res = GetStringFromCharArray(chars);
+Console.WriteLine(res);
