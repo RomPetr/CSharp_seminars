852ea1c09835008255e679eab5aa1470e3b90c06
Rompetr78 <rompetr@yahoo.com>
2024-02-20 00:14:53 +0800

    added the 3 task of /seminar_6/Program.cs

--- seminar_6/Program.cs
+++ seminar_6/Program.cs
@@ -21,7 +21,7 @@ string res = GetStringFromCharArray(chars);
 Console.WriteLine(res);
 */
 //---------------------------------------------------------------
-/* Задача 2 */
+/* Задача 2 
 // На основе символов строки (тип string) сформировать массив
 // символов (тип char[]). Вывести массив на экран.
 // Пример:
@@ -44,3 +44,37 @@ string str = "Hello world";
 Console.WriteLine($"Строчка исходная: {str}");
 char[] chars = ConvertStringToCharArray(str);
 Console.WriteLine($"Массив: [ {String.Join(";", chars)} ]");
+*/
+/* Задача 3 */
+// Считать строку с консоли, состоящую из латинских
+// букв в нижнем регистре. Выяснить, сколько среди
+// введённых букв гласных.
+// Пример
+// “hello” => 2
+// “world” => 1
+
+int GetVovelsCount (string str)
+{
+    // str = "hello", 'h' - 1 символ, char
+    string vovels = "aoueyi"; // Гласные буквы в англ. алфавите
+    int vovelsCount = 0; // Количество гласных 
+    foreach (char symbol in str) // Получает каждый символ введенной строчки
+    {
+        // 'a' == a, o, u, e, y, i
+        foreach (char vovel in vovels) // Получаем гласные
+        {
+            if (symbol == vovel) // Нашли гласную букву
+            {
+                vovelsCount++; // vovelsCount = vovelsCount + 1
+                break; // Если нашли гласную, переходим к след.букве
+            }
+        }
+    }
+    return vovelsCount;
+}
+
+Console.Write("Введите строчку: ");
+string inputString = Console.ReadLine();
+inputString = inputString.ToLower(); // нижний регистр, "HI" -> "hi"
+// ToUpper() - верхний ("hi" => "HI")
+Console.WriteLine($"В строчке: {inputString} в нижн. регистре : {GetVovelsCount(inputString)} гласных");
