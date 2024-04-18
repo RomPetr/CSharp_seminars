fabea060e3c5be28e37beaae87d04c879e942282
Rompetr78 <rompetr@yahoo.com>
2024-04-18 21:57:33 +0800

    добавил функцию CountArray в fin_program.cs

--- final_task/fin_program.cs
+++ final_task/fin_program.cs
@@ -1,21 +1,34 @@
-// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
-//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо 
+// Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк,
+//длина которых меньше, либо равна 3 символам. Первоначальный массив можно ввести с клавиатуры, либо
 //задать на старте выполнения алгоритма. При решении не рекомендуется пользоваться коллекциями, лучше обойтись исключительно массивами.
 // Примеры:
 // [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 // [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 // [“Russia”, “Denmark”, “Kazan”] → []
 
-void createArray (string[] array)
+//ввод массива с клавиатуры
+void createArray(string[] array)
 {
     for (int i = 0; i < array.Length; i++)
     {
         Console.Write($"Введите элемент под индексом {i}:\t ");
-        array[i] = Console.ReadLine();     
+        array[i] = Console.ReadLine();
     }
-    
 }
 
+// подсчет количества элементов массива с длиной строки меньше или равно 3
+int CountArray(string[] arr)
+{
+    int newSize = 0;
+    for (int i = 0; i < arr.Length; i++)
+    {
+        if (arr[i].Length <= 3)
+        {
+            newSize++;
+        }
+    }
+    return newSize;
+}
 
 Console.Write("Введите количество элементов массива\t");
 int N = Convert.ToInt32(Console.ReadLine());
@@ -24,7 +37,7 @@ createArray(inputArray);
 
 Console.WriteLine("Исходный массив:");
 
-for(int i = 0; i < inputArray.Length; i++)
+for (int i = 0; i < inputArray.Length; i++)
 {
     Console.Write(inputArray[i]);
     Console.Write("\t");
